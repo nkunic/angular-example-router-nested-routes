2c96ae351599bf266e4a036f5abd4539357ea6c5
nkunic <45877400+nkunic@users.noreply.github.com>
2023-05-28 19:52:21 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,8 @@ import { Component } from '@angular/core';
       <a routerLink="/user"
         [routerLinkActiveOptions]="{ exact: true }"
         routerLinkActive="active">User</a>
-      <a routerLink="/customer"
-        [routerLinkActiveOptions]="{ exact: true }"
-        routerLinkActive="active">Customer</a>
     </nav>
     <router-outlet></router-outlet>
-  `,
-  styles: [`
-    a:hover, a.active { color: red; }
-  `]
+  `
 })
 export class AppComponent {}
